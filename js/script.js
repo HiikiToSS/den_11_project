428e7a0296709e2897aa57e6e4d99ddb0fe7929d
that_who_fighting_for_2D <101421494+HiikiToSS@users.noreply.github.com>
2022-07-13 19:13:11 +0000

    сделал кнопку развернутого описания и начали работу над "о нас" и "контактами"

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let switchMode = document.getElementById("design_minimalism");
 
 switchMode.onclick = function () {
   let theme = document.getElementById("theme");
-  
+
   if(theme.getAttribute("href") == "static/main.css") {
     theme.href = "static/dark-mode.css";
   } else {
