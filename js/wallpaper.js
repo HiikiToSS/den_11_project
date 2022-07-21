8c34dd3f9087b540b44e6cf3ecbf810f3e2ff9b1
that_who_fighting_for_2D <101421494+HiikiToSS@users.noreply.github.com>
2022-07-21 20:01:15 +0000

    практически сделал тёмную тему, осталось только разобраться с картинками и возможно дизайном кнопки перекл темы + по теме юзера

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -4,7 +4,7 @@ let about_theme = document.getElementById("switch_about")
 let contacts_theme = document.getElementById("switch_contacts")
 let langs_theme = document.getElementById("switch_langs")
 
-let c_img = document.getElementById('c++')
+let c_img = document.getElementById('c_')
 let java_img  = document.getElementById('java')
 let js_img = document.getElementById('js')
 let kotlin_img = document.getElementById('kotlin')
@@ -36,13 +36,32 @@ function setThemeOnLoad(){
 }
 
 function setTheme1(){
+  if(about_theme){about_theme.href = '../static/dark-theme/dark-about.css'}
+  if(contacts_theme){contacts_theme.href = '../static/dark-theme/dark-contacts.css'}
+  if(langs_theme){langs_theme.href = '../static/dark-theme/dark-langs.css'}
+  
+  if(c_img){c_img.src = "../static/img/hi/_c++.png"}
+  if(swift_img){swift_img.src = "../static/img/hi/_swift.png"}
+  if(kotlin_img){kotlin_img.src = "../static/img/hi/_kotlin.png"}
+  if(java_img){java_img.src = "../static/img/hi/_java.png"}
+  if(js_img){js_img.src = "../static/img/hi/_js.png"}
+  if(python_img){python_img.src = "../static/img/hi/_python.png"}
+  
   nav_theme.href = '../static/dark-theme/navigation-dark.css'
   lang_theme.href = '../static/dark-theme/dark-language.css'
-
 }
 
 function setTheme2(){
   nav_theme.href = '../static/navigation.css'
   lang_theme.href = '../static/language.css'
+  if(about_theme){about_theme.href = '../static/about.css'}
+  if(langs_theme){langs_theme.href = '../static/langs.css'}
+  if(contacts_theme){contacts_theme.href = '../static/contacts.css'}
 
+  if(c_img){c_img.src = "../static/img/hi/l_c++.png"}
+  if(swift_img){swift_img.src = "../static/img/hi/l_swift.png"}
+  if(kotlin_img){kotlin_img.src = "../static/img/hi/l_kotlin.png"}
+  if(java_img){java_img.src = "../static/img/hi/l_js.png"}
+  if(js_img){js_img.src = "../static/img/hi/l_c++.png"}
+  if(python_img){python_img.src = "../static/img/hi/l_python.png"}
 }
