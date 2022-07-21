24ced73193d990b5769f5d392b1fb4912658e13d
that_who_fighting_for_2D <101421494+HiikiToSS@users.noreply.github.com>
2022-07-21 06:12:13 +0000

    отчаянно пытаюсь изменять тему везде  но ничерта не получается

--- js/wallpaper.js
+++ js/wallpaper.js
@@ -1,18 +1,48 @@
-let is = true
+let nav_theme = document.getElementById("switch_nav")
+let lang_theme = document.getElementById("switch_lang")
+let about_theme = document.getElementById("switch_about")
+let contacts_theme = document.getElementById("switch_contacts")
+let langs_theme = document.getElementById("switch_langs")
+
+let c_img = document.getElementById('c++')
+let java_img  = document.getElementById('java')
+let js_img = document.getElementById('js')
+let kotlin_img = document.getElementById('kotlin')
+let python_img = document.getElementById('python')
+let swift_img = document.getElementById('swift')
+
+
+setThemeOnLoad()
 
 function turn_night_on(){
-  let nav_theme = document.getElementById("switch_nav")
-  let lang_theme = document.getElementById("switch_lang");
-  if (is){
-    nav_theme.href = '../static/dark-theme/navigation-dark.css'
-    lang_theme.href = "../static/dark-theme/dark-language.css"
-    is = false
+  if (localStorage.getItem('theme') == "true"){
+    localStorage.setItem('theme', "false")
+    setTheme1()
   }
   else{
-    nav_theme.href = '../static/navigation.css'
-    lang_theme.href = "../static/language.css"
-    is = true
+    localStorage.setItem('theme', "true")
+    setTheme2()
   }
+   document.location.reload()
+}
 
-  //$('#styles').attr("href", "s.css")
+function setThemeOnLoad(){
+  if (localStorage.getItem('theme') == "true"){
+    setTheme1()
+  }
+  else{
+    setTheme2()
+  }
+}
+
+function setTheme1(){
+  nav_theme.href = '../static/dark-theme/navigation-dark.css'
+  lang_theme.href = '../static/dark-theme/dark-language.css'
+
+}
+
+function setTheme2(){
+  nav_theme.href = '../static/navigation.css'
+  lang_theme.href = '../static/language.css'
+
 }
